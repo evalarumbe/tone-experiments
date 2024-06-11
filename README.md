333198bc4cb9aecb00d3ff28257e6e2cc4c402dd
Eva Larumbe <evalarumbe@gmail.com>
2024-06-11 15:47:13 +0200

    Connect an effect: FeedbackDelay

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Tone.js Experiments
 
 Minimalist repo to experiment with Tone.js
+
+## Resources
+
+- [How To Use EFFECTS in Tone.js | Web Audio Tutorial - Pt.3](https://www.youtube.com/watch?app=desktop&v=UCNRRag2GgE&ab_channel=TheCodeCreative)
--- index.js
+++ index.js
@@ -5,11 +5,13 @@
 function init() {
   // Select DOM nodes
   const playToneButton = document.querySelector('#play-tone');
+  const fdToneButton = document.querySelector('#fd-tone');
   const psToneButton = document.querySelector('#ps-tone');
   const psAudioButton = document.querySelector('#ps-audio');
 
   // Add listeners
   playToneButton.addEventListener('click', playTone);
+  fdToneButton.addEventListener('click', playToneWithFeedbackDelay);
   psToneButton.addEventListener('click', pitchShiftTone);
   psAudioButton.addEventListener('click', pitchShiftAudio);
 }
@@ -20,11 +22,27 @@ window.addEventListener('load', init);
 /**
  * Lib
  */
-  
+
 function playTone() {
   //create a synth and connect it to the main output (your speakers)
   const synth = new Tone.Synth().toDestination();
   
+  //play a middle 'C' for the duration of an 8th note
+  synth.triggerAttackRelease('C4', '8n');
+}
+
+function playToneWithFeedbackDelay() {
+  //create a synth and connect it to the main output (your speakers)
+  const synth = new Tone.Synth();
+  const feedbackDelay = new Tone.FeedbackDelay();
+
+  // Routing concept
+  // synth -> effect -> output
+
+  // Think of .connect like an audio cable
+  synth.connect(feedbackDelay);
+  feedbackDelay.toDestination();
+
   //play a middle 'C' for the duration of an 8th note
   synth.triggerAttackRelease('C4', '8n');
 }
