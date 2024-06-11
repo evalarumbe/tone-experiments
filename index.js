2cc5758bcbeed0f0a93bcf6040b392d3237f0f0c
Eva Larumbe <evalarumbe@gmail.com>
2024-06-11 16:13:43 +0200

    Experiment with FeedbackDelay

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 /**
  * Init
+ * 
+ * Default bpm:     120
+ * Routing concept: synth -> effect -> output
  */
 
 function init() {
@@ -24,26 +27,32 @@ window.addEventListener('load', init);
  */
 
 function playTone() {
-  //create a synth and connect it to the main output (your speakers)
+  // create a synth and connect it to the main output (your speakers)
   const synth = new Tone.Synth().toDestination();
   
-  //play a middle 'C' for the duration of an 8th note
+  // play a middle 'C' for the duration of an 8th note
   synth.triggerAttackRelease('C4', '8n');
 }
 
 function playToneWithFeedbackDelay() {
-  //create a synth and connect it to the main output (your speakers)
+  // create a synth and connect it to the main output (your speakers)
   const synth = new Tone.Synth();
-  const feedbackDelay = new Tone.FeedbackDelay();
+  // delay the input for intervals of a dotted 8th note
+  // each delay plays at 70% of the volume of the previous
+  // const feedbackDelay = new Tone.FeedbackDelay("8n.", 0.7);
+  const feedbackDelay = new Tone.FeedbackDelay({
+    feedback: 0.3,
+    delayTime: 0.7,
+    // maxDelay: 2, // default is 1 sec. If you want greater delayTime, you must manually adjust maxDelay. 
+    // delayTime: 2, // maxDelay must be equal or larger to this number, otherwise you'll see an error saying it's "clamped" to the maxDelay value
+    wet: 0.7,
+  });
 
-  // Routing concept
-  // synth -> effect -> output
-
-  // Think of .connect like an audio cable
+  // think of .connect like an audio cable
   synth.connect(feedbackDelay);
   feedbackDelay.toDestination();
 
-  //play a middle 'C' for the duration of an 8th note
+  // play a middle 'C' for the duration of an 8th note
   synth.triggerAttackRelease('C4', '8n');
 }
 
